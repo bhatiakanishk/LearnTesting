860bf972c49dc899089cdf059bf6c44962cdfec0
Kanishk Bhatia <bhatia.ka@northeastern.edu>
2023-08-21 20:23:46 -0400

    Create shopping_cart.py

---
+++ 1. Pytest/shopping_cart.py
@@ -0,0 +1,20 @@
+from typing import List
+
+class ShoppingCart:
+    def __init__(self, max_size: int) -> None:
+        self.items: List[str] = []
+        self.max_size = max_size
+
+    def add(self, item: str):
+        if self.size() == self.max_size:
+            raise OverflowError("Cannot add more items")
+        self.items.append(item)
+
+    def size(self) -> int:
+        return len(self.items)
+
+    def get_items(self) -> List[str]: # type: ignore
+        return self.items
+
+    def get_total_price(self, price_map):
+        pass
