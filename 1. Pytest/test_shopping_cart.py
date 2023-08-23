430f370cc0aab7b6d87cbcbee38230e80bb7eb93
Kanishk Bhatia <bhatia.ka@northeastern.edu>
2023-08-22 20:31:45 -0400

    Update test_shopping_cart.py

--- 1. Pytest/test_shopping_cart.py
+++ 1. Pytest/test_shopping_cart.py
@@ -1,3 +1,5 @@
+from unittest.mock import Mock
+from item_database import ItemDatabase
 from shopping_cart import ShoppingCart
 import pytest
 
@@ -22,8 +24,13 @@ def test_when_add_more_than_max_items_should_fail(cart):
 def test_can_get_total_price(cart):
     cart.add("apple")
     cart.add("orange")
-    price_map = {
-        "apple": 1.0,
-        "orange": 2.0
-    }
-    assert cart.get_total_price(price_map) == 3.0
+    item_database = ItemDatabase()
+    
+    def mock_get_item(item: str):
+        if item == "apple":
+            return 1.0
+        if item == "orange":
+            return 2.0
+
+    item_database.get = Mock(side_effect = mock_get_item)
+    assert cart.get_total_price(item_database) == 3.0
